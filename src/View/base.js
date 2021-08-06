5938a679afdad4737cf59a75d1210f1c42c22cd0
auyshijain97 <ayushi9702@gmail.com>
2021-08-06 13:39:22 +0530

    implemented songs queue

--- src/View/base.js
+++ src/View/base.js
@@ -1,8 +1,10 @@
 import { loadPlaylist } from "../index.js";
+// window.loadPlaylist=loadPlaylist;
 export const element = {
   searchResList: document.querySelector(".trending_data"),
   searchList: document.querySelector(".row2"),
   neeraj: document.querySelector(".neeraj"),
+  songsQueueHeader: document.querySelector(".queue_title"),
 };
 
 export const elementStrings = {
@@ -24,7 +26,17 @@ export const clearLoader = () => {
     loader.parentElement.removeChild(loader);
   }
 }
-export  const limitTitleSize = (title, limit = 17) => {
+
+export const clearQueue = () => {
+  const songsQueueList = document.querySelector(".songs_queue");
+  if (songsQueueList) 
+  {
+    songsQueueList.remove();
+  }
+};
+
+
+export  const limitTitleSize = (title, limit = 15) => {
   const newTitle = [];
   if (title && title.length > limit) {
     title.split(" ").reduce((acc, cur) => {
@@ -42,25 +54,32 @@ export const renderResult = (data) => {
   const markup = `
          <div class="trending_image">
             <img src="${data.image}" alt="" class="trending_pic"/>
-            <h4 class="trending_title" value="${data.title}" >${limitTitleSize(data.title)}</h4>
+            <div class="playIcon">
+               <i class="fas fa-play fa-2x play playButton" value="${data.playlistID}" ></i>
+             </div>
+            <h4 class="trending_title">${limitTitleSize(data.title)}</h4>
             <p class="trending_author" >${limitTitleSize(data.singers)}</p>
             </div>`;
-            // document.querySelector(".trending_title").addEventListener("click",loadPlaylist);
             return markup;
   
 };
+export const renderSongsInQueue = (data) => {
+  const markup = `
+          <li>
+             <img src="${data.image}" alt="" class="queue_image"/>
+          </li>
+             `;
+        
+  return markup;
+}
+
+
+
 export const renderPage = (data) => {
       let firstRowData = data.slice(0,12);
-      // const markup1 = `
-      // <div class="trending-row">
-      //   ${firstRowData.forEach((data)=> {
-      //       renderResult(data);
-      //   })}
-      // </div>
-      //   `;
-        let insideDiv ='';
+      let insideDiv ='';
         for(let i=0;i<firstRowData.length;i++) {
-          insideDiv+=renderResult(firstRowData[i]);
+          insideDiv += renderResult(firstRowData[i]);
         }
         const markup1 = `
           <div class="trending-row">
@@ -77,9 +96,33 @@ export const renderPage = (data) => {
           <div class="trending-row">
             ${insideDiv}
           </div>
-        `;
-        
+        `; 
         element.neeraj.insertAdjacentHTML("beforeend", markup2);
+      
+        var anchors = document.querySelectorAll(".playButton");
+
+              for (var z = 0; z < anchors.length; z++) {
+                var elem = anchors[z];
+                elem.onclick = function () {
+                  return loadPlaylist(event);
+                };
+              }
+
+}
+export const renderSongsQueue= (queue) => {
+  let songsListHTML='';
+   queue.forEach(el => {
+     if(el.image){
+      songsListHTML+=renderSongsInQueue(el);
+     }
+   });
+   const markup = `
+          <ol class="songs_queue">
+            ${songsListHTML}
+          </ol>`;
+    console.log(markup);
+    element.songsQueueHeader.insertAdjacentHTML("afterend", markup);
+
 }
   
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import Trending from "./model/Trending.js";
 import BackendApi from "./model/backendApi.js";
-import { renderLoader, clearLoader, renderPage, limitTitleSize, renderResult, element} from "./View/base.js";
+import {
+  renderLoader,
+  clearLoader,
+  renderPage,
+  limitTitleSize,
+  renderSongsQueue,
+  clearQueue,
+} from "./View/base.js";
 // 1.)creating the audio tag
 const audioPlayer = document.createElement("audio");
 
 audioPlayer.volume = 0.5;
 let currentVolume = audioPlayer.volume;
 let isPlaying = false;
-let playBackRate=1.0;
-let doShuffle=false;
-let indexArray=[];
-//2.) Creating new object
+let playBackRate = 1.0;
+let doShuffle = false;
+let indexArray = [];
 const trending = new Trending();
-const backendApi = new BackendApi() 
 let songsQueue = [];
 const proxyUrl = `http://localhost:8080/`;
 
@@ -27,7 +32,7 @@ audioPlayer.addEventListener('loadedmetadata', function() {
 
 // 4.)audio playing currentTime
 audioPlayer.addEventListener('timeupdate', (event) => {
-    audioPlayer.playbackRate=playBackRate;
+    audioPlayer.playbackRate = playBackRate;
     const currentTime = Math.floor(audioPlayer.currentTime);
     const duration = Math.floor(audioPlayer.duration);
     // console.log("Current Time ", currentTime, " and Duration ", duration);
@@ -89,41 +94,38 @@ const prev = () => {
     showButton('.playing__pause');
 }
 const repeat = () => {
-    if(!audioPlayer.loop)
-    {
-        audioPlayer.loop=true;
-        audioPlayer.play(); 
+    if (!audioPlayer.loop) {
+        audioPlayer.loop = true;
+        audioPlayer.play();
         changeColor(".playing__repeat", "blue");
         changeFontSize(".repeat_icon_size", "27px");
-    }
-    else{
-        audioPlayer.loop=false;
+    } else {
+        audioPlayer.loop = false;
         audioPlayer.play();
         changeColor(".playing__repeat", "");
-        changeFontSize(".repeat_icon_size", ""); 
+        changeFontSize(".repeat_icon_size", "");
     }
-   
+
 }
-const changeColor = (className , color) => {
-    getElement(className).style.color=color;
+const changeColor = (className, color) => {
+    getElement(className).style.color = color;
 }
 
-const changeFontSize = (className , fontSize) => {
-    getElement(className).style.fontSize=fontSize;
-} 
+const changeFontSize = (className, fontSize) => {
+    getElement(className).style.fontSize = fontSize;
+}
 
 const shuffle = () => {
     doShuffle = !doShuffle;
     console.log("should shuffle?", doShuffle);
-    if(doShuffle){
+    if (doShuffle) {
         changeColor(".playing__shuffle", "blue");
-        changeFontSize(".shuffle_icon_size", "27px");  
-    }
-    else{
+        changeFontSize(".shuffle_icon_size", "27px");
+    } else {
         changeColor(".playing__shuffle", "");
-        changeFontSize(".shuffle_icon_size", "");  
+        changeFontSize(".shuffle_icon_size", "");
     }
-     
+
 }
 // Volume
 const onUnmuteClicked = () => {
@@ -154,7 +156,7 @@ const hideVolume = () => {
 }
 // DropDown Speed List 
 const playingSpeed = () => {
-   playBackRate = getElement(".dropDown").value;
+    playBackRate = getElement(".dropDown").value;
 }
 
 
@@ -175,32 +177,28 @@ const changePlayerCurrTime = (delta) => {
 }
 // shuffling the songs
 const getRandomValue = () => {
-         let nextRandomValue=0;
-            while((nextRandomValue=Math.floor(Math.random()*trending.url.length)) === indexOfMusicBeingPlayed) {}
-          return nextRandomValue;
+    let nextRandomValue = 0;
+    while ((nextRandomValue = Math.floor(Math.random() * trending.url.length)) === indexOfMusicBeingPlayed) {}
+    return nextRandomValue;
 }
 
 const getNextSong = () => {
-    if(doShuffle){
-            
-            indexArray.push(indexOfMusicBeingPlayed);
-          indexOfMusicBeingPlayed = getRandomValue();
+    if (doShuffle) {
 
-        for(let i=0;i<indexArray.length;i++)
-        {
-            if(indexArray[i]===indexOfMusicBeingPlayed)
-            {
-                indexOfMusicBeingPlayed=getRandomValue();
+        indexArray.push(indexOfMusicBeingPlayed);
+        indexOfMusicBeingPlayed = getRandomValue();
+
+        for (let i = 0; i < indexArray.length; i++) {
+            if (indexArray[i] === indexOfMusicBeingPlayed) {
+                indexOfMusicBeingPlayed = getRandomValue();
                 continue;
             }
-            if(indexArray.length===trending.url.length)
-            {
-                indexArray=[];
+            if (indexArray.length === trending.url.length) {
+                indexArray = [];
                 break;
             }
         }
-    }
-    else{
+    } else {
         indexOfMusicBeingPlayed = (indexOfMusicBeingPlayed + 1) % trending.url.length;
     }
     console.log("Playing ", indexOfMusicBeingPlayed);
@@ -219,98 +217,104 @@ const showButton = (buttonCss) => {
     document.querySelector(buttonCss).style.display = 'block';
 }
 
-async function getHomePage () {
-  let details = null;
-  if (localStorage.getItem("homepage")) {
-      details = JSON.parse(localStorage.getItem("homepage"));
-  } else {
-    console.log("Getting data");
-    try {
-      const result = await axios(
-        // proxyUrl + "https://ayushi-web-scrapper.herokuapp.com/data"
-        proxyUrl + "http://localhost:3000/metadata"
-        // proxyUrl +
-        //   "https://apg-saavn-api.herokuapp.com/playlist/?q=https://www.jiosaavn.com/featured/romantic-hits-2020---hindi/ABiMGqjovSFuOxiEGmm6lQ"
-      );
-    //   console.log(result);
-       details = result.data;
-       localStorage.setItem("homepage", JSON.stringify(details));
-    } catch (err) {
-      console.log(err);
+async function getHomePage() {
+    let details = null;
+    if (localStorage.getItem("homepage")) {
+        details = JSON.parse(localStorage.getItem("homepage"));
+    } else {
+        console.log("Getting data");
+        try {
+            const result = await axios(
+                proxyUrl + "https://ayushi-web-scrapper.herokuapp.com/metadata"
+            );
+            //   console.log(result);
+            details = result.data;
+            localStorage.setItem("homepage", JSON.stringify(details));
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
-  // console.log(data[0].title);
-  console.log(details);
-  renderPage(details);
+    // console.log(data[0].title);
+    console.log(details);
+    renderPage(details);
 }
 
-export const loadPlaylist = async(event)=> {
-        const playlistURL = event.srcElement.getAttribute("value");
-        console.log("Loading Playlist ", playlistURL);
-       renderLoader(document.querySelector(".playing__play"));
-       try {
-         const res = await axios(
-           proxyUrl +
-             `https://apg-saavn-api.herokuapp.com/playlist/?q=${playlistURL}`
-         );
-         console.log(res);
-         trending.url = [];
-         indexOfMusicBeingPlayed = 0;
-         clearLoader();
-         res.data.songs.forEach((song) => trending.url.push(song.media_url));
-         songsQueue = res.data.songs;
-           
-       }catch(err){
-           console.log(err);
-       }     
+export const loadPlaylist = async (event) => {
+    const playlistID = event.srcElement.getAttribute("value");
+    console.log("Loading Playlist ", playlistID);
+    renderLoader(document.querySelector(".playing__play"));
+    try {
+        const res = await axios(
+            proxyUrl +
+            `https://ayushi-web-scrapper.herokuapp.com/playlist/${playlistID}`
+        );
+        console.log(res);
+        trending.url = [];
+        indexOfMusicBeingPlayed = 0;
+        clearLoader();
+        clearQueue();
+        if (res.data.songs) {
+            res.data.songs.forEach((song) => trending.url.push(song.media_url));
+            songsQueue = res.data.songs;
+        } else if (res.data.song) {
+            trending.url.push(res.data.media_url);
+            songsQueue.push(res.data);
+        }
+            renderSongsQueue(songsQueue);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 const playTrack = (el) => {
     // setInterval(updateCurrTime, 1000);
     console.log(songsQueue);
-    if(songsQueue) {
-        getElement(".playing_image").setAttribute('src', songsQueue[indexOfMusicBeingPlayed].image);
-        getElement(".playing_title").innerHTML = limitTitleSize(songsQueue[indexOfMusicBeingPlayed].song);
-        getElement(".playing_author").innerHTML =
-          limitTitleSize(songsQueue[indexOfMusicBeingPlayed].singers);
-        
-    }
-    audioPlayer.src = el;
-    console.log(audioPlayer.duration);
-    var playPromise = audioPlayer.play();
-    if (playPromise !== undefined) {
-        playPromise.then(_ => {
-                // Automatic playback started!
-                // Show playing UI.
-            })
-            .catch(error => {
-                // Auto-play was prevented
-                // Show paused UI.
-            });
-    }
+    if (songsQueue) {
 
+        getElement(".playing_image").setAttribute(
+            "src",
+            songsQueue[indexOfMusicBeingPlayed].image
+        );
+        getElement(".playing_title").innerHTML = limitTitleSize(
+            songsQueue[indexOfMusicBeingPlayed].song
+        );
+        getElement(".playing_author").innerHTML = limitTitleSize(
+            songsQueue[indexOfMusicBeingPlayed].singers
+        );
+    }
+audioPlayer.src = el;
+console.log(audioPlayer.duration);
+var playPromise = audioPlayer.play();
+if (playPromise !== undefined) {
+    playPromise.then(_ => {
+            // Automatic playback started!
+            // Show playing UI.
+        })
+        .catch(error => {
+            // Auto-play was prevented
+            // Show paused UI.
+        });
 }
-
+}
 
 const getElement = (css) => {
     return document.querySelector(css);
 }
 
-window.addEventListener("load",getHomePage);
+window.addEventListener("load", getHomePage);
 document.querySelector(".playing__pause").addEventListener("click", pause);
 document.querySelector(".playing__play").addEventListener("click", play);
 document.querySelector(".playing__previous").addEventListener("click", prev);
 document.querySelector(".playing__next").addEventListener("click", next);
-document.querySelector(".playing__repeat").addEventListener("click",repeat);
-document.querySelector(".playing__shuffle").addEventListener("click",shuffle);
+document.querySelector(".playing__repeat").addEventListener("click", repeat);
+document.querySelector(".playing__shuffle").addEventListener("click", shuffle);
 document.querySelector(".playing__unmute").addEventListener("click", onUnmuteClicked);
 document.querySelector(".playing__mute").addEventListener("click", onMuteClicked);
 document.querySelector(".slider").addEventListener("change", changeTheTime);
 document.querySelector(".volume_slider").addEventListener("change", changeVolume);
-document.querySelector(".volume__button").addEventListener("mouseover",showVolume);
-document.querySelector(".volume__button").addEventListener("mouseout",hideVolume);
+document.querySelector(".volume__button").addEventListener("mouseover", showVolume);
+document.querySelector(".volume__button").addEventListener("mouseout", hideVolume);
 document.querySelector(".dropDown").addEventListener("change", playingSpeed);
-document.querySelector(".trending_title").addEventListener("click",loadPlaylist);
 document.addEventListener("keypress", function(event) {
     if (event.which === 32 || event.keyCode === 32) {
         if (isPlaying) {
